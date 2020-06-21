3750af4f4b5bf299f0ae8d49594aad7ad6ebe892
Youzhi Hu <jyzhu0101@gmail.com>
2020-06-21 14:17:13 +1000

    Using data attribute with jQuery

--- script.js
+++ script.js
@@ -3,33 +3,43 @@ setInterval(() => {
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 }, 1000);
 
+// Creating corresponding variables which means 9 = 9am - schedule
+const HOURS = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+const SCHEDULES = [
+    //
+    "9am-schedule",
+    "10am-schedule",
+    "11am-schedule",
+    "12pm-schedule",
+    "1pm-schedule",
+    "2pm-schedule",
+    "3pm-schedule",
+    "4pm-schedule",
+    "5pm-schedule",
+];
+
+const now = moment(); // Get the current time according to moment.js Docs.
+
 // Coloring the textarea block
-function render() {
-    const SCHEDULE = [
-        "9am-schedule",
-        "10am-schedule",
-        "11am-schedule",
-        "12pm-schedule",
-        "1pm-schedule",
-        "2pm-schedule",
-        "3pm-schedule",
-        "4pm-schedule",
-        "5pm-schedule",
-    ];
-
-    const HOUR = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-
-    const now = moment();
-
+function renderDisplay() {
     for (let i = 0; i < 9; i++) {
-        if (HOUR[i] < now.hour()) {
-            $("#" + SCHEDULE[i]).addClass("past");
-        } else if (HOUR[i] === now.hour()) {
-            $("#" + SCHEDULE[i]).addClass("present");
+        $("#" + SCHEDULES[i]).val(localStorage.getItem(SCHEDULES[i]));
+
+        if (HOURS[i] < now.hour()) {
+            $("#" + SCHEDULES[i]).addClass("past");
+        } else if (HOURS[i] === now.hour()) {
+            $("#" + SCHEDULES[i]).addClass("present");
         } else {
-            $("#" + SCHEDULE[i]).addClass("future");
+            $("#" + SCHEDULES[i]).addClass("future");
         }
     }
 }
 
-render();
+renderDisplay();
+
+// Saving plans to local storage by clicking button
+$(".saveBtn").on("click", function () {
+    const getID = $(this).data("id");
+    const plan = $("#" + getID + "-schedule").val();
+    localStorage.setItem(getID + "-schedule", plan);
+});
